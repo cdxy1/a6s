6c66fea1f17bc25e704209e84d42fc988ff36054
cdxy1 <dalyvinchi@gmail.com>
2026-03-05 03:20:02 +0300

    Upd app js

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 import express from "express";
+import userRouter from "./src/routes/userRoutes.js"
 
 const app = express()
-
-app.get("/", (req, res) => {
-    res.send("Kekes")
-})
+app.use("/users", userRouter)
 
 app.listen(3000)
