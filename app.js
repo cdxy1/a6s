10b73b802291e64259a2f7e8ffed64058944c756
cdxy1 <dalyvinchi@gmail.com>
2026-03-06 03:02:37 +0300

    Add middleware

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ import express from "express";
 import userRouter from "./src/routes/userRoutes.js"
 
 const app = express()
-app.use("/users", userRouter)
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/", userRouter)
+
 
 app.listen(3000)
